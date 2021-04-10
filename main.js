b47134a38bc1ef4cc87ce642c8abe1e59bfb06aa
fimtiaz4 <76715694+fimtiaz4@users.noreply.github.com>
2021-04-10 15:57:58 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.input_form').on('keyup', function (e) {
         if (e.keyCode == 13 && $('.input_form').val() != '') {
-            var task = $('<div class="task"></div>').text($('.input_form').val());
+            var task = $('<li class="task"></li>').text($('.input_form').val());
 
 
             var del = $('<i class=" fa fa-trash r"></i>').click(
@@ -17,19 +17,19 @@ $(document).ready(function () {
                 function () {
                     var p = $(this).parent();
                     p.fadeOut(function () {
-                       $('.completedTask').append(p);
-                       p.fadeIn();
+                        $('.completedTask ol').append(p);
+                        p.fadeIn();
                     });
                     $(this).remove();
                 });
- 
 
 
-    task.append(del , ok);
-    $('.taskToDo').append(task);
-    $('.input_form').val('');
 
-}
+            task.append(del, ok);
+            $('.taskToDo ol').append(task);
+            $('.input_form').val('');
+
+        }
 
 
     });
